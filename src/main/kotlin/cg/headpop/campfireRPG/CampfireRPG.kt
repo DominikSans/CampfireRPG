e85c776d4b9c78cfd7b2c6310a78933631c99bf8
Dominika <tovarza7@gmail.com>
2026-03-31 01:04:02 -0500

    v 1.0.0

---
+++ src/main/kotlin/cg/headpop/campfireRPG/CampfireRPG.kt
@@ -0,0 +1,14 @@
+package cg.headpop.campfireRPG
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CampfireRPG : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
